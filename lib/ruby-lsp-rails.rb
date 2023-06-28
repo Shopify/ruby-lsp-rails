c01433976d9510535a03ccace7063c4cd3ea18b7
Andy Waite <andyw8@users.noreply.github.com>
2023-06-28 11:24:41 -0400

    Add link to docs

--- lib/ruby-lsp-rails.rb
+++ lib/ruby-lsp-rails.rb
@@ -8,6 +8,10 @@ require "ruby_lsp_rails/version"
 require "ruby_lsp_rails/railtie"
 
 module RubyLsp
+  # # Supported features
+  #
+  # - [Hover](rdoc-ref:RubyLsp::Rails::Hover)
+  # - [CodeLens](rdoc-ref:RubyLsp::Rails::CodeLens)
   module Rails
   end
 end
