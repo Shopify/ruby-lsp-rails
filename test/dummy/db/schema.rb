9b3e770ae0efbc5afc113587a1707bea4f1bd341
Vinicius Stock <vinicius.stock@shopify.com>
2023-10-19 14:03:35 -0400

    Add namespaced model for testing purposes

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_202955) do
+ActiveRecord::Schema[7.2].define(version: 2023_10_19_180159) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
