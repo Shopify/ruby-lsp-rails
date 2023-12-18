abe92294dbf9c3ce1409b4b2a8c63262cb191556
Syed Faraaz Ahmad <faraaz98@live.com>
2023-12-18 20:01:36 +0530

    add types

--- lib/ruby_lsp/ruby_lsp_rails/schema_collector.rb
+++ lib/ruby_lsp/ruby_lsp_rails/schema_collector.rb
@@ -1,21 +1,31 @@
-class SchemaCollector < Prism::Visitor
-  attr_reader :tables
+# typed: strict
+# frozen_string_literal: true
 
-  def initialize
-    @tables = {}
+module RubyLsp
+  class SchemaCollector < Prism::Visitor
+    extend T::Sig
 
-    super
-  end
+    sig { returns(T::Hash(String, Prism::Location)) }
+    attr_reader :tables
 
-  def visit_call_node(node)
-    return if node.block.nil?
+    sig { void }
+    def initialize
+      @tables = {}
 
-    node.block.body.child_nodes.each do |child_node|
-      next unless child_node.is_a?(Prism::CallNode)
-      next unless child_node.name == :create_table
+      super
+    end
 
-      table_name = child_node.arguments.child_nodes.first.content
-      @tables[table_name.classify] = child_node.location
+    sig { params(node: Prism::CallNode).void }
+    def visit_call_node(node)
+      return if node.block.nil?
+
+      node.block.body.child_nodes.each do |child_node|
+        next unless child_node.is_a?(Prism::CallNode)
+        next unless child_node.name == :create_table
+
+        table_name = child_node.arguments.child_nodes.first.content
+        @tables[table_name.classify] = child_node.location
+      end
     end
   end
 end
