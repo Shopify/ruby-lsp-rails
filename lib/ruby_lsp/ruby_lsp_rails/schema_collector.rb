e3f748fac54e7a2713325fed07056cb27dc00ba7
Syed Faraaz Ahmad <faraaz98@live.com>
2023-12-18 20:04:27 +0530

    add RubyLsp::Rails module

--- lib/ruby_lsp/ruby_lsp_rails/schema_collector.rb
+++ lib/ruby_lsp/ruby_lsp_rails/schema_collector.rb
@@ -2,29 +2,31 @@
 # frozen_string_literal: true
 
 module RubyLsp
-  class SchemaCollector < Prism::Visitor
-    extend T::Sig
+  module Rails
+    class SchemaCollector < Prism::Visitor
+      extend T::Sig
 
-    sig { returns(T::Hash[String, Prism::Location]) }
-    attr_reader :tables
+      sig { returns(T::Hash[String, Prism::Location]) }
+      attr_reader :tables
 
-    sig { void }
-    def initialize
-      @tables = {}
+      sig { void }
+      def initialize
+        @tables = {}
 
-      super
-    end
+        super
+      end
 
-    sig { params(node: Prism::CallNode).void }
-    def visit_call_node(node)
-      return if node.block.nil?
+      sig { params(node: Prism::CallNode).void }
+      def visit_call_node(node)
+        return if node.block.nil?
 
-      node.block.body.child_nodes.each do |child_node|
-        next unless child_node.is_a?(Prism::CallNode)
-        next unless child_node.name == :create_table
+        node.block.body.child_nodes.each do |child_node|
+          next unless child_node.is_a?(Prism::CallNode)
+          next unless child_node.name == :create_table
 
-        table_name = child_node.arguments.child_nodes.first.content
-        @tables[table_name.classify] = child_node.location
+          table_name = child_node.arguments.child_nodes.first.content
+          @tables[table_name.classify] = child_node.location
+        end
       end
     end
   end
