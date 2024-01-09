c10176919bf256ef2490a6555be5ecafa4956eb8
Andy Waite <andyw8@users.noreply.github.com>
2024-01-09 12:36:23 -0500

    Set migration version to 7.1

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2023_10_19_180159) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_19_180159) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
