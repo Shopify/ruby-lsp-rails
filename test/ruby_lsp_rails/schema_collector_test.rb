0e414a7050c2fc978ab690b28d51ea60fc4a0671
Syed Faraaz Ahmad <faraaz98@live.com>
2024-01-31 22:25:38 +0530

    fix: move schema file into class

--- test/ruby_lsp_rails/schema_collector_test.rb
+++ test/ruby_lsp_rails/schema_collector_test.rb
@@ -3,19 +3,19 @@
 
 require "test_helper"
 
-SCHEMA_FILE = <<~RUBY
-  ActiveRecord::Schema[7.1].define(version: 2023_12_09_114241) do
-    create_table "cats", force: :cascade do |t|
-    end
-
-    create_table "dogs", force: :cascade do |t|
-    end
-  end
-RUBY
-
 module RubyLsp
   module Rails
     class SchemaCollectorTest < ActiveSupport::TestCase
+      SCHEMA_FILE = <<~RUBY
+        ActiveRecord::Schema[7.1].define(version: 2023_12_09_114241) do
+          create_table "cats", force: :cascade do |t|
+          end
+
+          create_table "dogs", force: :cascade do |t|
+          end
+        end
+      RUBY
+
       test "store locations of models by parsing create_table calls" do
         collector = RubyLsp::Rails::SchemaCollector.new(Pathname.new("example_app"))
         Prism.parse(SCHEMA_FILE).value.accept(collector)
