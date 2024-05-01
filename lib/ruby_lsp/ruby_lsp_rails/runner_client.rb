3f5fcbe73778f0aec7dd1961b65630930dd718b3
Andy Waite <andyw8@users.noreply.github.com>
2024-05-01 11:29:12 -0400

    Use Bundler.with_original_env

--- lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
+++ lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
@@ -48,14 +48,10 @@ module RubyLsp
           # If we can't set the session ID, continue
         end
 
-        stdin, stdout, stderr, wait_thread = Bundler.with_unbundled_env do
-          Open3.popen3(
-            "bin/rails",
-            "runner",
-            "#{__dir__}/server.rb",
-            "start",
-          )
+        stdin, stdout, stderr, wait_thread = Bundler.with_original_env do
+          Open3.popen3("bin/rails", "runner", "#{__dir__}/server.rb", "start")
         end
+
         @stdin = T.let(stdin, IO)
         @stdout = T.let(stdout, IO)
         @stderr = T.let(stderr, IO)
