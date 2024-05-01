cb8857ad589d897d40f1950bb2ef949fdf01ea17
Andy Waite <andy.waite@shopify.com>
2024-05-01 11:52:20 -0400

    Remove Sorbet from `server.rb` (#339)
    * Remove Sorbet from `server.rb`
    
    * Use Bundler.with_original_env

--- lib/ruby_lsp/ruby_lsp_rails/server.rb
+++ lib/ruby_lsp/ruby_lsp_rails/server.rb
@@ -1,23 +1,7 @@
-# typed: strict
+# typed: false
 # frozen_string_literal: true
 
-require "sorbet-runtime"
 require "json"
-
-begin
-  T::Configuration.default_checked_level = :never
-  # Suppresses call validation errors
-  T::Configuration.call_validation_error_handler = ->(*) {}
-  # Suppresses errors caused by T.cast, T.let, T.must, etc.
-  T::Configuration.inline_type_error_handler = ->(*) {}
-  # Suppresses errors caused by incorrect parameter ordering
-  T::Configuration.sig_validation_error_handler = ->(*) {}
-rescue
-  # Need this rescue so that if another gem has
-  # already set the checked level by the time we
-  # get to it, we don't fail outright.
-  nil
-end
 
 # NOTE: We should avoid printing to stderr since it causes problems. We never read the standard error pipe from the
 # client, so it will become full and eventually hang or crash. Instead, return a response with an `error` key.
@@ -27,16 +11,12 @@ module RubyLsp
     class Server
       VOID = Object.new
 
-      extend T::Sig
-
-      sig { void }
       def initialize
         $stdin.sync = true
         $stdout.sync = true
-        @running = T.let(true, T::Boolean)
+        @running = true
       end
 
-      sig { void }
       def start
         initialize_result = { result: { message: "ok" } }.to_json
         $stdout.write("Content-Length: #{initialize_result.length}\r\n\r\n#{initialize_result}")
@@ -54,19 +34,13 @@ module RubyLsp
         end
       end
 
-      sig do
-        params(
-          request: String,
-          params: T.nilable(T::Hash[Symbol, T.untyped]),
-        ).returns(T.any(Object, T::Hash[Symbol, T.untyped]))
-      end
       def execute(request, params)
         case request
         when "shutdown"
           @running = false
           VOID
         when "model"
-          resolve_database_info_from_model(T.must(params).fetch(:name))
+          resolve_database_info_from_model(params.fetch(:name))
         when "reload"
           ::Rails.application.reloader.reload!
           VOID
@@ -79,7 +53,6 @@ module RubyLsp
 
       private
 
-      sig { params(model_name: String).returns(T::Hash[Symbol, T.untyped]) }
       def resolve_database_info_from_model(model_name)
         const = ActiveSupport::Inflector.safe_constantize(model_name)
         unless active_record_model?(const)
@@ -105,7 +78,6 @@ module RubyLsp
         { error: e.full_message(highlight: false) }
       end
 
-      sig { params(const: T.untyped).returns(T::Boolean) }
       def active_record_model?(const)
         !!(
           const &&
