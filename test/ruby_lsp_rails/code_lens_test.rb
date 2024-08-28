ba99254a9710993be7afe9a57cac26cc370ce2d7
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-08-28 15:38:03 -0400

    Preperation for server addons

--- test/ruby_lsp_rails/code_lens_test.rb
+++ test/ruby_lsp_rails/code_lens_test.rb
@@ -384,7 +384,7 @@ module RubyLsp
 
       def generate_code_lens_for_source(source, file: "/fake.rb")
         with_server(source, URI(file)) do |server, uri|
-          sleep(0.1) while RubyLsp::Addon.addons.first.instance_variable_get(:@client).is_a?(NullClient)
+          sleep(0.1) while RubyLsp::Addon.addons.first.instance_variable_get(:@rails_runner_client).is_a?(NullClient)
 
           server.process_message(
             id: 1,
