428c18cc525406f46a3e695155dcf9e6b4d98691
Vinicius Stock <vinicius.stock@shopify.com>
2024-09-05 16:17:05 -0400

    Upgrade Sorbet

--- lib/ruby_lsp/ruby_lsp_rails/support/location_builder.rb
+++ lib/ruby_lsp/ruby_lsp_rails/support/location_builder.rb
@@ -11,18 +11,18 @@ module RubyLsp
           sig { params(location_string: String).returns(Interface::Location) }
           def line_location_from_s(location_string)
             *file_parts, line = location_string.split(":")
-
-            raise ArgumentError, "Invalid location string given" unless file_parts
+            raise ArgumentError, "Invalid location string given" if file_parts.empty?
 
             # On Windows, file paths will look something like `C:/path/to/file.rb:123`. Only the last colon is the line
             # number and all other parts compose the file path
             file_path = file_parts.join(":")
+            line_as_number = line ? Integer(line.to_i) - 1 : 0
 
             Interface::Location.new(
               uri: URI::Generic.from_path(path: file_path).to_s,
               range: Interface::Range.new(
-                start: Interface::Position.new(line: Integer(line) - 1, character: 0),
-                end: Interface::Position.new(line: Integer(line) - 1, character: 0),
+                start: Interface::Position.new(line: line_as_number, character: 0),
+                end: Interface::Position.new(line: line_as_number, character: 0),
               ),
             )
           end
