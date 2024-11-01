165dd54b2c07424a9cfd64ef345e2ecb383e4d5c
Vinicius Stock <vinicius.stock@shopify.com>
2024-11-01 14:19:41 -0400

    Check for pending migrations when watcher is fired

--- test/ruby_lsp_rails/addon_test.rb
+++ test/ruby_lsp_rails/addon_test.rb
@@ -49,6 +49,49 @@ module RubyLsp
         addon = Addon.new
         addon.workspace_did_change_watched_files(changes)
       end
+
+      test "handling window show message response to run migrations" do
+        RunnerClient.any_instance.expects(:run_migrations).once.returns({ message: "Ran migrations!", status: 0 })
+        outgoing_queue = Thread::Queue.new
+        global_state = GlobalState.new
+        global_state.apply_options({ capabilities: { window: { workDoneProgress: true } } })
+
+        addon = Addon.new
+        addon.activate(global_state, outgoing_queue)
+
+        # Wait until activation is done
+        Thread.new do
+          addon.rails_runner_client
+        end.join
+
+        addon.handle_window_show_message_response("Run Migrations")
+
+        progress_request = pop_message(outgoing_queue) { |message| message.is_a?(Request) }
+        assert_instance_of(Request, progress_request)
+
+        progress_begin = pop_message(outgoing_queue) do |message|
+          message.is_a?(Notification) && message.method == "$/progress"
+        end
+        assert_equal("begin", progress_begin.params.value.kind)
+
+        report_log = pop_message(outgoing_queue) do |message|
+          message.is_a?(Notification) && message.method == "window/logMessage"
+        end
+        assert_equal("Ran migrations!", report_log.params.message)
+
+        progress_report = pop_message(outgoing_queue) do |message|
+          message.is_a?(Notification) && message.method == "$/progress"
+        end
+        assert_equal("report", progress_report.params.value.kind)
+        assert_equal("Ran migrations!", progress_report.params.value.message)
+
+        progress_end = pop_message(outgoing_queue) do |message|
+          message.is_a?(Notification) && message.method == "$/progress"
+        end
+        assert_equal("end", progress_end.params.value.kind)
+      ensure
+        T.must(outgoing_queue).close
+      end
     end
   end
 end
