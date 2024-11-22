db6c55d51ef211b10726213b58a69cd4fc4497d5
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-11-22 13:54:22 -0500

    Remove list of supported features (now covered in jekyll docs)

--- lib/ruby-lsp-rails.rb
+++ lib/ruby-lsp-rails.rb
@@ -2,14 +2,3 @@
 # frozen_string_literal: true
 
 require "ruby_lsp_rails/version"
-
-module RubyLsp
-  # # Supported features
-  #
-  # - [Hover](rdoc-ref:RubyLsp::Rails::Hover)
-  # - [CodeLens](rdoc-ref:RubyLsp::Rails::CodeLens)
-  # - [DocumentSymbol](rdoc-ref:RubyLsp::Rails::DocumentSymbol)
-  # - [Definition](rdoc-ref:RubyLsp::Rails::Definition)
-  module Rails
-  end
-end
