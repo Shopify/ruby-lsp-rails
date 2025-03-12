d17e66959af977e81ed70c45e46fab652487fdb6
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-03-12 12:12:46 -0400

    wip

--- lib/ruby_lsp/ruby_lsp_rails/discover_tests.rb
+++ lib/ruby_lsp/ruby_lsp_rails/discover_tests.rb
@@ -5,19 +5,78 @@ module RubyLsp
   module Rails
     class DiscoverTests
       include Requests::Support::Common
+      DYNAMIC_REFERENCE_MARKER = T.let("<dynamic_reference>", String)
 
       # @override
       #: (ResponseBuilders::TestCollection response_builder, Prism::Dispatcher dispatcher, URI::Generic uri) -> void
       def initialize(response_builder, dispatcher, uri)
         @response_builder = response_builder
+        @nesting = T.let([], T::Array[String])
+        @uri = uri
         dispatcher.register(
           self,
+          :on_class_node_enter,
+          :on_class_node_leave,
+          :on_module_node_enter,
+          :on_module_node_leave,
+          :on_def_node_enter, # for 'classic' tests... do we need to handle here?
           :on_call_node_enter, # e.g. `test "..."`
+          :on_call_node_leave,
         )
+      end
+
+      #: (Prism::ClassNode node) -> void
+      def on_class_node_enter(node)
+        name = constant_name(node.constant_path)
+        name ||= name_with_dynamic_reference(node.constant_path)
+
+        @nesting << name
+      end
+
+      #: (Prism::ClassNode node) -> void
+      def on_class_node_leave(node)
+        @nesting.pop
+      end
+
+      #: (Prism::ModuleNode node) -> void
+      def on_module_node_enter(node)
+        name = constant_name(node.constant_path)
+        name ||= name_with_dynamic_reference(node.constant_path)
+
+        @nesting << name
+      end
+
+      #: (Prism::ModuleNode node) -> void
+      def on_module_node_leave(node)
+        @nesting.pop
       end
 
       #: (Prism::CallNode node) -> void
       def on_call_node_enter(node)
+        return unless node.name == :test
+        return unless node.block
+
+        args = node.arguments&.arguments
+        return unless args
+        return unless args.first.is_a?(Prism::StringNode)
+
+        description = T.must(args[0]).unescaped # right way to access?
+
+        test_item = Requests::Support::TestItem.new(
+          description,
+          description,
+          @uri,
+          range_from_node(node),
+          tags: [:active_support_declarative],
+        )
+
+        @response_builder.add(test_item)
+      end
+
+      #: ((Prism::ConstantPathNode | Prism::ConstantReadNode | Prism::ConstantPathTargetNode | Prism::CallNode | Prism::MissingNode) node) -> String
+      def name_with_dynamic_reference(node)
+        slice = node.slice
+        slice.gsub(/((?<=::)|^)[a-z]\w*/, DYNAMIC_REFERENCE_MARKER)
       end
     end
   end
--- test/ruby_lsp_rails/discover_tests_test.rb
+++ test/ruby_lsp_rails/discover_tests_test.rb
@@ -3,15 +3,87 @@
 
 require "test_helper"
 
+# TODO: the tests pass individually but fail when all run together, find out why
+# TODO: see code_lens_test.rb for possible things to handle
+
 module RubyLsp
-  # TODO: convert to a declarative test, using ActiveSupport::TestCase
-  class DiscoverTestsTest < Minitest::Test
+  class DiscoverTestsTest < ActiveSupport::TestCase # Minitest::Test
     include RubyLsp::TestHelper
 
-    def test_handles_minitest_tests_that_extend_active_support_declarative
+    # def test_active_support_declarative
+    test "active support declarative" do
       source = <<~RUBY
         class MyTest < ActiveSupport::TestCase
-          def test_something; end
+          test "hello world" do
+          end
+        end
+      RUBY
+
+      with_active_support_declarative_tests(source) do |items|
+        assert_equal(1, items.size)
+        assert_equal("hello world", items[0][:label])
+        assert_equal([:active_support_declarative], items[0][:tags])
+      end
+    end
+
+    test "ignores methods not named test" do
+      source = <<~RUBY
+        class MyTest < ActiveSupport::TestCase
+          foo "something else" do
+          end
+        end
+      RUBY
+
+      with_active_support_declarative_tests(source) do |items|
+        assert_empty(items)
+      end
+    end
+
+    test "ignores tests without a block" do
+      source = <<~RUBY
+        class MyTest < ActiveSupport::TestCase
+          test "something else"
+        end
+      RUBY
+
+      with_active_support_declarative_tests(source) do |items|
+        assert_empty(items)
+      end
+    end
+
+    test "ignores tests with a non-string name argument" do
+      source = <<~RUBY
+        class MyTest < ActiveSupport::TestCase
+          test foo do
+          end
+        end
+      RUBY
+
+      with_active_support_declarative_tests(source) do |items|
+        assert_empty(items)
+      end
+    end
+
+    test "ignores test cases without a name" do
+      source = <<~RUBY
+        class MyTest < ActiveSupport::TestCase
+          test do
+          end
+        end
+      RUBY
+
+      with_active_support_declarative_tests(source) do |items|
+        assert_empty(items)
+      end
+    end
+
+    test "recognizes plain test cases" do
+      skip "not yet"
+
+      source = <<~RUBY
+        class MyTest < ActiveSupport::TestCase
+          def test_foo
+          end
         end
       RUBY
 
@@ -21,6 +93,7 @@ module RubyLsp
     end
 
     def with_active_support_declarative_tests(source, &block)
+      puts "a1"
       with_server(source) do |server, uri|
         server.global_state.index.index_single(uri, <<~RUBY)
           module Minitest
@@ -50,13 +123,13 @@ module RubyLsp
     end
 
     def get_response(server)
-      server.pop_response
-      server.pop_response
-      result = server.pop_response
-
-      # if result.is_a?(Error)
-      #   flunk(result.message)
-      # end
+      result = nil
+      until result.is_a?(RubyLsp::Result)
+        result = server.pop_response
+        if result.is_a?(Error)
+          flunk(result.message)
+        end
+      end
 
       result.response
     end
