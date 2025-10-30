0bc8c58c3efb222dbe0ae7215a1b240bacfa6a12
Cassio Godinho <cassiopgodinho@gmail.com>
2025-10-30 15:16:06 -0300

    users table now has one more index, add that new index to the assertion

--- test/ruby_lsp_rails/runner_client_test.rb
+++ test/ruby_lsp_rails/runner_client_test.rb
@@ -58,7 +58,10 @@ module RubyLsp
           ]
         end
         foreign_keys = ["country_id"]
-        indexes = [{ name: "index_users_on_country_id", columns: ["country_id"], unique: false }]
+        indexes = [
+          { name: "users_unique_complex", columns: "COALESCE(country_id, 0), ltrim(first_name)", unique: true },
+          { name: "index_users_on_country_id", columns: ["country_id"], unique: false }
+        ]
         response = @client.model("User") #: as !nil
         assert_equal(columns, response.fetch(:columns))
         assert_equal(foreign_keys, response.fetch(:foreign_keys))
